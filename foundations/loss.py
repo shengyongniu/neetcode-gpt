07869e584f69a8e53000c63a8afa0749df59427e
Simon Sheng-Yong Niu <niu.shengyong@gmail.com>
2026-07-10 08:52:26 +0800

    Update Cross-entropy loss (Math Foundations 4/6)

--- foundations/loss.py
+++ foundations/loss.py
@@ -4,10 +4,21 @@ from numpy.typing import NDArray
 
 class Solution:
 
-    def softmax(self, z: NDArray[np.float64]) -> NDArray[np.float64]:
-        # z is a 1D NumPy array of logits
-        # Hint: subtract max(z) for numerical stability before computing exp
-        # return np.round(your_answer, 4)
-        z = z - np.max(z)
-        softmax = np.exp(z) / np.sum(np.exp(z), keepdims = True)
-        return np.round(softmax, 4)
+    def binary_cross_entropy(self, y_true: NDArray[np.float64], y_pred: NDArray[np.float64]) -> float:
+        # y_true: true labels (0 or 1)
+        # y_pred: predicted probabilities
+        # Hint: add a small epsilon (1e-7) to y_pred to avoid log(0)
+        # return round(your_answer, 4)
+        # y logp + (1-y)log(1-p)
+        y_pred += 1e-7
+        loss = -np.mean(y_true * np.log(y_pred) + (1-y_true) * np.log(1-y_pred))
+        return round(loss, 4)
+
+    def categorical_cross_entropy(self, y_true: NDArray[np.float64], y_pred: NDArray[np.float64]) -> float:
+        # y_true: one-hot encoded true labels (shape: n_samples x n_classes)
+        # y_pred: predicted probabilities (shape: n_samples x n_classes)
+        # Hint: add a small epsilon (1e-7) to y_pred to avoid log(0)
+        # return round(your_answer, 4)
+        loss = -1* np.mean(np.sum(y_true * np.log(y_pred), keepdims = True, axis = 1))
+        return round(loss, 4)
+
