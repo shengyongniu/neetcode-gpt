ea6593a281b6e3d7f1d884307e1e7ce7e6a4e3dc
Simon Sheng-Yong Niu <niu.shengyong@gmail.com>
2026-08-03 17:00:30 +0800

    Update BPE tokenizer (Build GPT 1/10)

--- data/tokenizer.py
+++ data/tokenizer.py
@@ -3,46 +3,31 @@ from typing import List
 
 class Solution:
     def get_merges(self, corpus: str, num_merges: int) -> List[List[str]]:
+        # 1. Split corpus into a list of individual characters
+        # 2. For each merge step:
+        #    a. Count frequency of all adjacent token pairs
+        #    b. Find the most frequent pair (break ties lexicographically)
+        #    c. Merge all non-overlapping occurrences left to right
+        #    d. Record the merge as [token_a, token_b]
+        # 3. Return the list of merges performed
         from collections import Counter
-
         char_list = list(corpus)
         merges = []
-
         for _ in range(num_merges):
             if len(char_list) < 2:
                 break
-
             freq_counter = Counter()
-            for i in range(len(char_list) - 1):
-                freq_counter[(char_list[i], char_list[i + 1])] += 1
-
-            # Highest frequency, lexicographically smallest on ties
-            most_common_pair = min(
-                freq_counter,
-                key=lambda pair: (-freq_counter[pair], pair)
-            )
-
-            merges.append([
-                most_common_pair[0],
-                most_common_pair[1]
-            ])
-
-            new_char_list = []
+            for i in range(len(char_list)-1):
+                freq_counter[(char_list[i],char_list[i+1])] += 1
+            # most_common_list = freq_counter.most_common()
+            # most_common_list.sort(key = lambda x: (-x[1], x[0])))
+            # most_common_pair = most_common_list[0][0]
+            most_common_pair = min(freq_counter, key = lambda pair: (-freq_counter[pair], pair))
+            merges.append([most_common_pair[0], most_common_pair[1]])
             i = 0
-
-            while i < len(char_list):
-                if (
-                    i + 1 < len(char_list)
-                    and (char_list[i], char_list[i + 1]) == most_common_pair
-                ):
-                    new_char_list.append(
-                        char_list[i] + char_list[i + 1]
-                    )
-                    i += 2
-                else:
-                    new_char_list.append(char_list[i])
-                    i += 1
-
-            char_list = new_char_list
+            while i < len(char_list)-1:
+                if (char_list[i], char_list[i+1]) == most_common_pair:
+                    char_list[i:i+1+1] = ["".join(char_list[i:i+1+1])]
+                i += 1
 
         return merges
